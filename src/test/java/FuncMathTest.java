0b86ef38732c92491402751de1ef93d1a3bd0cd7
cherrydan <cherrydan.1978@gmai.com>
2022-07-22 17:28:47 +0300

    committed JUnitTestExample

--- src/test/java/FuncMathTest.java
+++ src/test/java/FuncMathTest.java
@@ -17,21 +17,31 @@ public class FuncMathTest {
         math = null;
     }
 
+    //Проверка на выданный функцией числовой результат
     @Test
     public void calls()  {
         math.factorial(1);
-        Assertions.assertEquals(1, math.getCalls());
+        Assertions.assertEquals(5, math.getCalls());
 
-        math.factorial(5);
-        Assertions.assertEquals(2, math.getCalls());
+//        math.factorial(5);
+//        Assertions.assertEquals(4, math.getCalls());
+    }
+
+    //Возвращает true если соответствует тому что мы ожидаем
+    @Test
+    public void factorial() {
+        Assertions.assertTrue(math.factorial(0) == 1);
+        Assertions.assertTrue(math.factorial(1) == 1);
+        Assertions.assertTrue(math.factorial(5) == 120);
     }
 
     @Test
-    public void factorial() {
-
-
-
+    public void factorialNegative(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            math.factorial(-2);
+        });
     }
+
 
 
 }
