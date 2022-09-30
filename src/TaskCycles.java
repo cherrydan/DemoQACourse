11b26c9c8cde5654fd98beb72d811238b9019c30
cherrydan <cherrydan.1978@gmail.com>
2022-09-30 13:52:05 +0300

    Put demo from stepik Java-course called TaskCycles

--- src/TaskCycles.java
+++ src/TaskCycles.java
@@ -1,5 +1,15 @@
-public class TaskCycles {
-    //3. Напечатать таблицу соответствия между весом в фунтах и весом в килограммах для
-    //значений 1, 2, ..., 10 фунтов (1 фунт = 453 г).
-    //4. Вычислить сумму 1+1/2+1/3+...+1/n
+//задача с курса по Java на Степик, считает количество введённых слов изпользуя метод сканера hasNext
+import java.util.Scanner;
+class TaskCycles {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int counter = 0;
+        System.out.println("Начните вводить слова каждое с новой строки (сигнал конца ввода в IDE - Ctrl+D --> ");
+        while(sc.hasNextLine()) { //пока есть следующий ввод - вводим слово и считаем
+            String word = sc.nextLine();
+            counter++;
+        }
+        System.out.println("Слов введено = " + counter);
+
+    }
 }
