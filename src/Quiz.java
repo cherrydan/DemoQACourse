04c7b949116ac5d7e2e129685fa47877f22d44f1
cherrydan <cherrydan.1978@gmail.com>
2022-10-15 11:44:49 +0300

    Quiz example from Alexey Vladykin

---
+++ src/Quiz.java
@@ -0,0 +1,17 @@
+/**
+ * Program that displays security word, created with MD5 algo
+ */
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
+public class Quiz {
+
+    public static void main(String[] args) throws Exception {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] digest = md.digest("abracadabra".getBytes(StandardCharsets.UTF_8));
+        for (byte b : digest) {
+            System.out.printf("%02x", b);
+        }
+    }
+}
