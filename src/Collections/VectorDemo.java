2f74de3f6015f429056071423b68fb7c14cd36e0
cherrydan <cherrydan.1978@gmail.com>
2023-02-08 13:35:19 +0300

    Rewrite in Java LeetCode task

---
+++ src/Collections/VectorDemo.java
@@ -0,0 +1,6 @@
+package Collections;
+//Vector очень похож на ArrayList, но он более старый, чем ArrayList
+// Отличие в том, что Vector синхронизированный, как и StringBuffer
+public class VectorDemo {
+
+}
